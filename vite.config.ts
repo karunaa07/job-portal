51c20d01a1678f3b5bca754d29e6bb2b74245fe3
karunaa07 <kashilkarkaruna@gmail.com>
2025-08-04 15:03:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    base: '/job-portal/', 
   },
 });
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/job-portal/', // 👈 this must match your repo name
+})
